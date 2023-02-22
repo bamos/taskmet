d1fd0c91423f62978eedc9c3f06250640670f4f2
Dishank Bansal <dishankbansal09@gmail.com>
2023-02-22 13:07:44 -0800

    added model pred polot

--- LODLs/plot-cubic.py
+++ LODLs/plot-cubic.py
@@ -8,29 +8,34 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 
 from IPython.core import ultratb
-sys.excepthook = ultratb.FormattedTB(mode='Verbose',
-                                        color_scheme='Linux',
-                                        call_pdb=1)
+
+sys.excepthook = ultratb.FormattedTB(mode="Verbose", color_scheme="Linux", call_pdb=1)
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('exp_root', type=str)
-    parser.add_argument('--pkl_tag', type=str, default='latest')
+    parser.add_argument("exp_root", type=str)
+    parser.add_argument("--pkl_tag", type=str, default="latest")
     args = parser.parse_args()
 
-    exp_path = f'{args.exp_root}/{args.pkl_tag}.pkl'
+    exp_path = f"{args.exp_root}/{args.pkl_tag}.pkl"
     assert os.path.exists(exp_path)
-    print('-- loading exp')
-    with open(exp_path, 'rb') as f:
+    print("-- loading exp")
+    with open(exp_path, "rb") as f:
         exp = pkl.load(f)
-    print('-- done')
+    print("-- done")
 
     # import ipdb; ipdb.set_trace()
     X_test, Y_test, Y_test_aux = exp.problem.get_test_data()
+
     fig, ax = plt.subplots()
     ax.scatter(X_test[0].ravel(), Y_test[0].ravel())
-    import ipdb; ipdb.set_trace()
-    fig.savefig('t.png')
 
-if __name__ == '__main__':
+    y_pred = exp.model(X_test[0].to("cuda")).cpu().detach().numpy()
+    ax.scatter(X_test[0].ravel(), y_pred.ravel())
+
+    fig.savefig("t.png")
+
+
+if __name__ == "__main__":
     main()
